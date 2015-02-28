318e58d75cc3fba46f3458ca783c3d8cf5cf9ad9
Nate Irwin <nate@nateirwin.net>
2015-02-28 14:24:42 -0700

    Expose marker to drawstart event handler

--- src/draw/handler/Draw.Feature.js
+++ src/draw/handler/Draw.Feature.js
@@ -23,9 +23,11 @@ L.Draw.Feature = L.Handler.extend({
 
 		this.fire('enabled', { handler: this.type });
 
-		console.log(this);
-
-		this._map.fire('draw:drawstart', { layerType: this.type });
+		if (this.type === 'marker') {
+			this._map.fire('draw:drawstart', { layerType: this.type, shape: this._marker });
+		} else {
+			this._map.fire('draw:drawstart', { layerType: this.type });
+		}
 	},
 
 	disable: function () {
